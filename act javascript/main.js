e6041264e177159a5bf2c2b548a621638f83152b
gguzman8 <gguzman8@ucol.mx>
2023-09-20 18:11:42 -0600

    Primera versión

---
+++ act javascript/main.js
@@ -0,0 +1,30 @@
+let velocidad= 98.5;   
+console.log(typeof velocidad) ;
+
+let userName = 'gaby';
+console.log(typeof userName) ;
+
+let isActive = true;
+console.log(typeof isActive) ;
+
+let nombre;
+console.log(typeof nombre) ;
+
+let edad; null;
+console.log(typeof edad) ;
+
+let z= 3n ** 55n;
+console.log(typeof z) ;
+
+let clave = Symbol('sym');
+console.log(typeof clave) ;
+
+let carros = ['toyota', 200, true];
+console.log(typeof carros) ;
+
+let x = [2, 4];
+let y = x;
+y.push(5);
+
+console.log(x);
+console.log(y);
